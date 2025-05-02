4e4af6caf74ac9cf52982a88257dd540ca1cd509
Lenora04 <it23309838@my.sliit.lk>
2025-05-02 20:26:38 +0530

    initial commit

---
+++ routes/inventoryRoutes.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const multer = require("multer"); //to handle image uploading
+
+
+
+const {
+  createInventoryItem,
+  getAllInventoryItems,
+  getInventoryItemById,
+  updateInventoryItem,
+  deleteInventoryItem,
+  getLowStockItems,
+  addUsageToInventoryItem,//********************* */
+  getInventoryUsageStats,//********************* */
+} = require("../controllers/inventoryController");
+
+const router = express.Router();
+
+// Set up storage for Multer
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "uploads/");
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + "-" + file.originalname);
+  },
+});
+
+const upload = multer({ storage: storage });
+
+// Route to create a new inventory item with image upload
+router.post("/add", upload.single("image"), createInventoryItem);
+
+// Route to get all inventory items
+router.get("/", getAllInventoryItems);
+//************ */
+router.get("/usage-stats", getInventoryUsageStats);
+router.get("/low-stock", getLowStockItems);
+
+
+// Route to get a single inventory item by ID
+router.get("/:id", getInventoryItemById);
+
+// Route to update an inventory item by ID (with optional image upload)
+router.put("/:id", upload.single("image"), updateInventoryItem);
+
+// Route to delete an inventory item by ID
+router.delete("/:id", deleteInventoryItem);
+
+// Route to get items below threshold (low stock)
+
+//************************************* */
+router.post("/:id/add-usage", addUsageToInventoryItem);
+
+
+//**************************** */
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,21 +1,38 @@
 const express = require("express");
+//const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const cors = require("cors");
 const connectDB = require("./db.js");
-const appointmentRoutes = require("./routes/appointmentRoutes");
-const userRoutes = require("./routes/userRoutes");
+const inventoryRoutes = require("./routes/inventoryRoutes");
 
+
+
+
+// Load environment variables
 dotenv.config();
 
+// Initialize Express
 const app = express();
 
-app.use(cors());
-app.use(express.json());
-app.use("/api/users", userRoutes);
-app.use("/api/appointments", appointmentRoutes);
-
+// Connect to MongoDB
 connectDB();
 
+// Middlewarev 
+app.use(cors());
+app.use(express.json());
+
+// Use the inventory routes
+app.use("/inventory", inventoryRoutes);
+app.use('/uploads', express.static('uploads'));
+
+// Sample route
+app.get("/", (req, res) => {
+  console.log("bye");
+  res.send("Welcome to Express App!");
+});
+// Define port
 const PORT = process.env.PORT || 5000;
 
+// Start the server
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
